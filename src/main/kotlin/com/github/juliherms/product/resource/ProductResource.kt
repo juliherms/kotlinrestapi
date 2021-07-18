7cdca0ae744e58f8b9867974d85781896c6acbc2
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2021-07-18 07:51:44 -0300

    implements products resource

--- src/main/kotlin/com/github/juliherms/product/resource/ProductResource.kt
+++ src/main/kotlin/com/github/juliherms/product/resource/ProductResource.kt
@@ -1,9 +1,8 @@
 package com.github.juliherms.product.resource
 
+import com.github.juliherms.product.model.Product
 import com.github.juliherms.product.service.ProductService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 /**
  * This class responsible to expose products to API
@@ -12,6 +11,33 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("v1/products")
 class ProductResource(private val productService: ProductService) {
 
+    /**
+     * List all products
+     */
     @GetMapping
     fun findAll() = productService.findAll()
+
+    /**
+     * Return product by id
+     */
+    @GetMapping("/{id}")
+    fun findById(@PathVariable  id:String) = productService.findById(id)
+
+    /**
+     * Save product
+     */
+    @PostMapping
+    fun save(@RequestBody product: Product) = productService.save(product)
+
+    /**
+     * Update product
+     */
+    @PutMapping
+    fun update(@RequestBody product:Product) = productService.update(product)
+
+    /**
+     * Delete product
+     */
+    @DeleteMapping("/{id}")
+    fun deleteById(@PathVariable id:String) = productService.deleteById(id)
 }
